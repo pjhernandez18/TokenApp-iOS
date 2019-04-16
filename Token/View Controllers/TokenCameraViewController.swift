6c1c673e1fd340968c89e0fdec2f35e5a92b9bea
pjhernandez18 <pjhernan@usc.edu>
2019-04-16 00:56:22 -0700

    able to download videos from Firebase and view on app

--- Token/View Controllers/TokenCameraViewController.swift
+++ Token/View Controllers/TokenCameraViewController.swift
@@ -8,9 +8,15 @@
 
 import UIKit
 import FirebaseAuth
+import FirebaseStorage
+import AVFoundation
+import AVKit
 
 class TokenCameraViewController: CameraViewController, UploadRecordingDelegate {
     
+    
+    var avPlayer: AVPlayer?
+    var avPlayerController = AVPlayerViewController()
     
     @IBOutlet weak var previewView: PreviewView!
     
@@ -42,6 +48,15 @@ class TokenCameraViewController: CameraViewController, UploadRecordingDelegate {
         changeCamera()
     }
     
+    @IBAction func downloadSnap(_ sender: Any) {
+        
+        print("downloading snap")
+        
+        //playVideo()
+        downloadVideo()
+        
+        
+    }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?){
         
         //need to change to TripsVC later
@@ -59,6 +74,63 @@ class TokenCameraViewController: CameraViewController, UploadRecordingDelegate {
         
         performSegue(withIdentifier: "UsersVC", sender: ["videoURL": videoURL])
     }
-  
+    
+    func playVideo(url: NSURL){
+        
+        //let fileURL = NSURL(string: "http://www.ebookfrenzy.com/ios_book/movie.mov")!
+        avPlayer = AVPlayer(url: url as URL)
+        
+        self.avPlayerController.player = avPlayer
+        self.addChild(avPlayerController)
+        self.view.addSubview(avPlayerController.view)
+        self.avPlayerController.view.frame = self.view.frame
+        
+        avPlayer?.play()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(TokenCameraViewController.playerDidFinish), name: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: avPlayer!.currentItem)
+        
+    }
+    
+    @objc func playerDidFinish() {
+        self.dismiss(animated: true, completion: nil)
+        avPlayerController.view.removeFromSuperview()
+        NotificationCenter.default.removeObserver(self)
+        
+    }
+    
+    func downloadVideo() {
+        let videoName = "videos/EE360C3E-D1A3-484B-9624-D85AC70CA044file:/private/var/mobile/Containers/Data/Application/1DFC767B-84C0-4BD2-ADC5-CFC5F7E01825/tmp/42DD8DFC-EFE4-4E00-9358-F4549DF3FBB7.mov"
+        let videoURL = DatabaseService.instance.storageRef.child(videoName)
+        
+        videoURL.downloadURL(completion: { (url, error) in
+            
+            if error != nil {
+                 print(error?.localizedDescription)
+                 print("returning")
+                 return
+            }
+            print(videoURL)
+            // self.playVideo()
+            // Otherwise, you can start downloading the file you want
+            Storage.storage().reference(forURL: String(describing: videoURL)).getMetadata { (metadata, error) in
+                if error != nil {
+                    print("error getting metadata")
+                } else {
+                    videoURL.downloadURL { (url, error) in
+                        guard let downloadUrl = url else {
+                            print("trouble getting the download url")
+                            return
+                        }
+                        print(downloadUrl)
+                        
+                        if downloadUrl != nil {
+                            print("download url had been obtained and set")
+                            self.playVideo(url: downloadUrl as! NSURL)
+                        }
+                    }
+            }
+            
+        }
+    })
 }
-
+}
